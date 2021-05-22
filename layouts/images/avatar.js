e088cd785c97c06335af876223521260e9e9c2d0
Jessi <74846260+eli56t@users.noreply.github.com>
2021-05-22 11:08:20 -0400

    Update avatar.js

--- layouts/images/avatar.js
+++ layouts/images/avatar.js
@@ -1,9 +1,9 @@
-$('.image_cover').each(function(){
-    var imageWidth = $(this).find('img').width();
-    var imageheight = $(this).find('img'). height();
-    if(imageWidth > imageheight){
-        $(this).addClass('landscape');
-    }else{
-        $(this).addClass('potrait');
-   }
+$('.image_cover').each(function() {
+  const imageWidth = $().find('img').width();
+  const imageheight = $().find('img').height();
+  if (imageWidth > imageheight) {
+    $().addClass('landscape');
+  } else {
+    $().addClass('potrait');
+  }
 });
