16f5644a029b26dbdd0194c09aca5cd844e470da
Jessi <74846260+eli56t@users.noreply.github.com>
2021-05-22 23:03:18 -0400

    .

--- layouts/images/avatar.js
+++
@@ -1,9 +0,0 @@
-$('.image_cover').each(function() {
-  const imageWidth = $().find('img').width();
-  const imageheight = $().find('img').height();
-  if (imageWidth > imageheight) {
-    $().addClass('landscape');
-  } else {
-    $().addClass('potrait');
-  }
-});
